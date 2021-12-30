a6be22d2bb637ee583868f40676c31f93aaf082f
k0mmander <96887208+k0mmander@users.noreply.github.com>
2021-12-30 20:24:50 +0100

    Create kwikarch.sh

---
+++ kwikarch.sh
@@ -0,0 +1,20 @@
+echo I'm updating the system! You might be prompted to enter your password once
+sudo pacman -Syu -y
+echo Done!
+echo Installing yay
+sudo pacman -S yay -y
+echo Done!
+echo Installing Neofetch
+yay -S neofetch -y
+echo Done!
+echo Installing BashTop
+yay -S bashtop
+echo Done!
+echo Installing Git!
+yay -S git -y
+echo Done!
+echo Installing Firefox
+yay -S firefox -y
+echo Done!
+echo Thanks for using kwikinstall!
+echo v0.0.0.1 pre
